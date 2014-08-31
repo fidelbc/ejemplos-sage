31424c8eced8218ffc06234e068edb1b0e872fb8
Rafael Villarroel <rvf0068@gmail.com>
2014-08-31 12:35:13 -0500

    Initial version with whitelake theme

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/ejemplos-sage/"
+http_images_path = "/ejemplos-sage/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/ejemplos-sage/fonts"
+css_dir = "public/ejemplos-sage/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
